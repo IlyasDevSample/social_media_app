cce0d45e43151b5b445758ed900f393e59fc5fbe
ilyas <elaissiilyas@gmail.com>
2023-01-16 18:08:36 +0100

    second commit

--- social_media_app/pages/index.tsx
+++ social_media_app/pages/index.tsx
@@ -3,7 +3,7 @@ import { Inter } from '@next/font/google'
 export default function Home() {
     return (
         <h1 className="text-3xl font-bold underline text-blue-400">
-            Hello World
+            Hello World! from Next.js
         </h1>
     )
 }
