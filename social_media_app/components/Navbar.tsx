3db6710211f473133ae0fcb401c4ec408933be5b
ilyas <elaissiilyas@gmail.com>
2023-01-19 13:28:12 +0100

    Next auth setup

--- social_media_app/components/Navbar.tsx
+++ social_media_app/components/Navbar.tsx
@@ -5,9 +5,14 @@ import { useRouter } from 'next/router'
 import { AiOutlineLogout } from "react-icons/ai";
 import { BiSearch } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
+import { useSession } from 'next-auth/react';
 
 
 const Navbar = () => {
+    const { data: session }= useSession()
+    console.log(session)
+
+
     return (
         <div className="border-b-2 border-gray-200">
             <div className='w-full flex justify-between items-center  py-2 px-5'>
@@ -16,6 +21,14 @@ const Navbar = () => {
                         <Image className='cursor-pointer' src="/images/brand-logo.png" alt="TIKI TAKA logo" width={300} height={70} />
                     </div>
                 </Link>
+
+                <div className="">
+                    Search
+                </div>
+
+                <div>
+                    User Profile
+                </div>
             </div>
         </div>
 
--- social_media_app/pages/api/auth/[...nextauth].ts
+++ social_media_app/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,14 @@
 import NextAuth from "next-auth"
 import FacebookProvider from "next-auth/providers/facebook"
 import GoogleProvider from "next-auth/providers/google"
+import { type } from "os"
+import { client as sanityClient } from "../../../utils/sanityClient"
 
+type User = {
+    _id: string,
+    userName: string,
+    imageUrl: string,
+}
 
 export default NextAuth({
     providers: [
@@ -10,4 +17,39 @@ export default NextAuth({
             clientSecret: process.env.GOOGLE_SECRET as string,
         }),
     ],
+    callbacks: {
+        async signIn({ user, account, profile }) {
+            const { email, image } = user
+
+            const userExists: User[] = await sanityClient
+                .fetch(`*[_type == "user" && userName== "${email}"]{_id, userName, imageURL}`)
+
+            if (userExists.length === 0) {
+                const newUser = {
+                    _type: 'user',
+                    userName: email,
+                    imageURL: image,
+                }
+                await sanityClient.create(newUser)
+            }
+
+            return true
+        },
+        async jwt({ token, user }) {
+            // add properties to token on sign in 
+            const userDb: User[] = await sanityClient
+                .fetch(`*[_type == "user" && userName== "${user?.email}"]{_id, userName, imageURL}`)
+
+            if (userDb.length > 0) {
+                // add provider user id to token
+                token.id = userDb[0]._id
+            }
+            return token
+        },
+        async session({ session, token }: any) {
+            // add property to session from token
+            session.user.id = token.id
+            return session
+        },
+    }
 })
