4c4323aa9ce734434f15a1bfa1e0cbeb16a384ae
ilyas <elaissiilyas@gmail.com>
2023-01-21 20:33:22 +0100

    hook

--- social_media_app/hooks/useWindowDimensions.ts
+++ social_media_app/hooks/useWindowDimensions.ts
@@ -2,29 +2,29 @@ import { useState, useEffect } from 'react';
 
 export default function useWindowDimensions() {
 
-  const hasWindow = typeof window !== 'undefined';
+    const hasWindow = typeof window !== 'undefined';
 
-  function getWindowDimensions() {
-    const width = hasWindow ? window.innerWidth : 10;
-    const height = hasWindow ? window.innerHeight : 10;
-    return {
-      width,
-      height,
-    };
-  }
-
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-
-  useEffect(() => {
-    if (hasWindow) {
-      const handleResize = () => {
-        setWindowDimensions(getWindowDimensions());
-      }
-
-      window.addEventListener('resize', handleResize);
-      return () => window.removeEventListener('resize', handleResize);
+    function getWindowDimensions() {
+        const width = hasWindow ? window.innerWidth : 10;
+        const height = hasWindow ? window.innerHeight : 10;
+        return {
+            width,
+            height,
+        };
     }
-  }, [hasWindow]);
 
-  return windowDimensions;
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+    useEffect(() => {
+        if (hasWindow) {
+            const handleResize = () => {
+                setWindowDimensions(getWindowDimensions());
+            }
+
+            window.addEventListener('resize', handleResize);
+            return () => window.removeEventListener('resize', handleResize);
+        }
+    }, [hasWindow]);
+
+    return windowDimensions;
 }
